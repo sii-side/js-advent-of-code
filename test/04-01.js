4d7f25ce8c935137e52e8448e67def2d9d01354e
sii <sii_side@yahoo.co.jp>
2018-02-11 18:48:29 +0900

    Solved Day 4

--- test/04-01.js
+++ test/04-01.js
@@ -1,18 +1,17 @@
 /* global describe, it */
 
-const solve = require('../2017/03-01.js')
+const solve = require('../2017/04-01.js')
 
 const chai = require('chai')
 const should = chai.should()
 
 const example = {
-  1: 0,
-  12: 3,
-  23: 2,
-  1024: 31
+  'aa bb cc dd ee': 1,
+  'aa bb cc dd aa': 0,
+  'aa bb cc dd aaa': 1
 }
 
-describe('03-01 exapmles', () => {
+describe('04-01 exapmles', () => {
   Object.keys(example).forEach(key => {
     it(`${key} -> ${example[key]}`, () => {
       solve(key).should.equal(example[key])
--- test/04-02.js
+++ test/04-02.js
@@ -1,18 +1,19 @@
 /* global describe, it */
 
-const solve = require('../2017/03-01.js')
+const solve = require('../2017/04-02.js')
 
 const chai = require('chai')
 const should = chai.should()
 
 const example = {
-  1: 0,
-  12: 3,
-  23: 2,
-  1024: 31
+  'abcde fghij': 1,
+  'abcde xyz ecdab': 0,
+  'a ab abc abd abf abj': 1,
+  'iiii oiii ooii oooi oooo': 1,
+  'oiii ioii iioi iiio': 0
 }
 
-describe('03-01 exapmles', () => {
+describe('04-02 exapmles', () => {
   Object.keys(example).forEach(key => {
     it(`${key} -> ${example[key]}`, () => {
       solve(key).should.equal(example[key])
